6ac91100803a76e9194cf1d4ea733f1edd2b3332
derkach <derkach@devweb03.lbdaq.cern.ch>
2015-08-07 11:50:06 +0200

    new features for labels and texts

--- webmonitor/static/javascripts/lib/d3.chart.histograms.js
+++ webmonitor/static/javascripts/lib/d3.chart.histograms.js
@@ -839,10 +839,10 @@
       config.color = '#000000';
     }
     if (config.x === undefined) {
-      config.x = 70;
+      config.x = 0;
     }
     if (config.y === undefined) {
-      config.y = 20;
+      config.y = 0;
     }
     return {
       name: name,
@@ -850,6 +850,9 @@
       xDomain: function() { return []; },
       yDomain: function() { return []; },
       draw: function(axes, g, transition) {
+        config.x = config.x + axes.margins.left;
+        config.y = config.y + axes.margins.top;
+
         if (arguments.length === 0) {
           console.error('Cannot draw ' + this.name + ', no arguments given');
           return;
@@ -861,6 +864,21 @@
         // Create 'background' rectangle
         var width = 150,
             height = this.data.length*20 + 10;
+
+        //maximum width
+        if (width>axes.width()) width = axes.width();
+        //move to the left if the name is too long
+        if (config.x + width > axes.margins.left+axes.width()) config.x = axes.margins.left + axes.width() - width;
+        //still be visible
+        if(config.x<0) config.x = 0;
+
+        //maximum height
+        if (height>axes.height()) height = axes.height();
+        //move down if the name is too long
+        if (config.y + width > axes.margins.top+axes.height()) config.y = axes.margins.top + axes.height() - height;
+        //still be visible
+        if(config.y<0) config.y = 0;
+
         g.selectAll('rect').data([null]).enter()
           .append('rect')
           .attr('x', 0)
@@ -1308,10 +1326,13 @@
       config.color = '#000000';
     }
     if (config.x === undefined) {
-      config.x = 70;
+      config.x = 0;
     }
     if (config.y === undefined) {
       config.y = 20;
+    }
+    if (config.bkg === undefined) {
+      config.bkg = "transparent";
     }
     return {
       name: name,
@@ -1326,23 +1347,9 @@
         if (transition === undefined) {
           transition = false;
         }
+        //put margin to x coordinate:
+        config.x = config.x + axes.margins.left;
         g.classed('TextBox', true);
-        // Create 'background' rectangle
-        var width = this.data.length*9+10,
-            height = 20;
-        g.selectAll('rect').data([null]).enter()
-          .append('rect')
-          .attr('x', 0)
-          .attr('y', -19)
-          .attr('width', width)
-          .attr('height', height)
-          .style('fill', '#ffffff')
-          .style('stroke', '#000000')
-          .style('stroke-width', '1px');
-        // Create join data, one <text> element per datum
-//        var join = g.selectAll('g').data("");
-//        join.enter().append('g')
-//          .classed('legend-item', true);
         g.selectAll('text').data([null])
           .enter()
           .append('text')
@@ -1353,6 +1360,26 @@
           .style("text-anchor", "start")
           .style('fill', config.color)
           .text(this.data);
+        // Create 'background' rectangle
+        var width = g.selectAll('text').node().getComputedTextLength()+10,
+            height = 20;
+        //maximum width
+        if (width>axes.width()) width = axes.width();
+        //move to the left if the name is too long
+        if (config.x + width > axes.margins.left+axes.width()) config.x = axes.margins.left + axes.width() - width;
+        //still be visible
+        if(config.x<0) config.x = 0;
+        g.selectAll('rect').data([null]).enter()
+          .append('rect')
+          .attr('x', 0)
+          .attr('y', -19)
+          .attr('width', width)
+          .attr('height', height)
+          .style('fill', config.bkg)
+          .style('stroke', '#000000')
+          .style('stroke-width', '1px');
+        g.selectAll('text').moveToFront();
+        g.classed('legend-item', true);
         // Set up dragging on the container element
         var initPosition = g.data()[0] === undefined ? [{x: config.x, y: config.y}] : g.data();
         g.data(initPosition);
@@ -1368,6 +1395,12 @@
       }
     };
   };
+
+d3.selection.prototype.moveToFront = function() {
+  return this.each(function(){
+    this.parentNode.appendChild(this);
+  });
+};
 
   d3.plotable = d3.plotable || {};
   d3.plotable.LabelBox = LabelBox;
